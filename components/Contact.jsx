ef539f5d91e84708357cf77112533f2acea39493
SALVADORPOETA <dsml@live.com.mx>
2023-01-12 08:23:53 -0600

    deploy12

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -9,10 +9,10 @@ import ContactImg from '../public/assets/contact.jpg';
 
 const Contact = () => {
   return (
-    <div id='contact' className='w-full lg:h-screen' >
+    <div id='contact' className='w-full lg:h-screen ml-10' >
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full' >
-            <p className='text-xl tracking-widest uppercase text-[#5651e5] ml-10' >Contact</p>
-            <h2 className='py-4 ml-10' >Get In Touch</h2>
+            <p className='text-xl tracking-widest uppercase text-[#5651e5]' >Contact</p>
+            <h2 className='py-4' >Get In Touch</h2>
             <div className='grid lg:grid-cols-5 gap-8' >
                 {/* left */}
                 <div className='col-span-3 lg:col-span-2 w-fit h-full shadow-xl shadow-gray-400 rounded-xl p-4' >
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,8 +9,8 @@ import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full' >
-        <div className='max-w-[1240px] mx-auto px-2 py-16 ml-10' >
+    <div id='projects' className='w-full ml-10' >
+        <div className='max-w-[1240px] mx-auto px-2 py-16' >
             <p className='text-xl tracking-widest uppercase text-[#5651e5]' >Projects</p>
             <h2 className='py-4' >What I've built</h2>
             <div className='grid md:grid-cols-2 gap-8' >
