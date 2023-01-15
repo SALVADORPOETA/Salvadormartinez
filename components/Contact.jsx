68d631977d0515d7cc80d1589af0461fa0daa095
SALVADORPOETA <dsml@live.com.mx>
2023-01-15 06:14:15 -0600

    contact2

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,7 +8,10 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import ContactImg from '../public/assets/contact.jpg';
 
 const Contact = () => {
-  return (
+    const Reset = () => {
+        reset();
+    }
+    return (
     <div id='contact' className='w-full lg:h-screen' >
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full' >
             <p className='text-xl tracking-widest uppercase text-[#5651e5]' >Contact</p>
@@ -16,6 +19,7 @@ const Contact = () => {
             <div className='grid lg:grid-cols-5 gap-8' >
                 {/* left */}
                 <div className='col-span-3 lg:col-span-2 w-fit h-full shadow-xl shadow-gray-400 rounded-xl p-4' >
+                    <div className='lg:p-4 h-full '>
                         <div>
                             <Image
                                 className='rounded-xl hover:scale-105 ease-in duration-300'
@@ -28,33 +32,116 @@ const Contact = () => {
                             <p>Front-End Developer</p>
                             <p className='py-4' >I am available for freelance or full-time positions. Contact me and let's talk.</p>
                         </div>
-                        <p className='uppercase pt-8' >Connect With Me</p>
-                        <div className='flex items-center justify-center py-4'>
-                            <a href='https://www.linkedin.com/in/salvador-mart%C3%ADnez-11211020a' target='_blank'>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
-                                    <FaLinkedinIn />
-                                </div>
-                            </a>
-                            <a href='https://github.com/SALVADORPOETA' target='_blank' >
+                        <div>
+                            <p className='uppercase pt-8' >Connect With Me</p>
+                            <div className='flex items-center justify-center py-4'>
+                                <a href='https://www.linkedin.com/in/salvador-mart%C3%ADnez-11211020a' target='_blank'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                </a>
+                                <a href='https://github.com/SALVADORPOETA' target='_blank' >
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
+                                        <FaGithub />
+                                    </div>
+                                </a>
+                                {/* 
                                 <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
-                                    <FaGithub />
+                                    <AiOutlineMail />
                                 </div>
-                            </a>
-                            {/* 
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
-                                <AiOutlineMail />
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
+                                    <BsFillPersonLinesFill />
+                                </div>
+                                */}
                             </div>
-                            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
-                                <BsFillPersonLinesFill />
-                            </div>
-                            */}
                         </div>
+                    </div>
                 </div>
             
                 {/* right */}
- 
-                <div className='col-span-3 h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4 ' >
+                 <div className='col-span-3 h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4 ' >
                     <div className='p-4'>
+                        <form
+                            action='https://getform.io/f/70877771-e309-418b-8fb8-d56d4a42203e' 
+                            method="POST"
+                            enctype="multipart/form-data"
+                            >
+                            <div className='grid md:grid-cols-2 gap-4 w-full py-2' >
+                                <div className='flex flex-col' >
+                                    <label className='uppercase text-sm py-2' >Name</label>
+                                    <input 
+                                        className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                        type="text" 
+                                        name='name'
+                                    />
+                                </div>
+                                <div className='flex flex-col' >
+                                    <label className='uppercase text-sm py-2' >Phone Number</label>
+                                    <input 
+                                        className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                        type="text"
+                                        name='phone' 
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex flex-col py-2' >
+                                <label className='uppercase text-sm py-2' >Email</label>
+                                <input 
+                                    className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                    type="email" 
+                                    name='email'
+                                />
+                            </div>
+                            <div className='flex flex-col py-2' >
+                                <label className='uppercase text-sm py-2' >Subject</label>
+                                <input 
+                                    className='border-2 rounded-lg p-3 flex border-gray-300' 
+                                    type="text"
+                                    name='subject' 
+                                />
+                            </div>
+                            <div className='flex flex-col py-2' >
+                                <label className='uppercase text-sm py-2' >Message</label>
+                                <textarea 
+                                    className='border-2 rounded-lg p-3 border-gray-300' 
+                                    rows='10'
+                                    name='message' />
+                            </div>
+                            <button className='w-full p-4 text-gray-100 mt-4' >Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center py-12' >
+                <Link href='/' >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300' >
+                        <HiOutlineChevronDoubleUp className='m-auto text-[#5651e5]' size={30} />  
+                    </div>
+                </Link>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Contact
+
+{/*
+<div className='flex items-center justify-center py-4'>
+    <a href='https://www.linkedin.com/in/salvador-mart%C3%ADnez-11211020a' target='_blank'>
+        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+            <FaLinkedinIn />
+        </div>
+    </a>
+    <a href='https://github.com/SALVADORPOETA' target='_blank' >
+        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
+            <FaGithub />
+        </div>
+    </a>
+</div>
+ */}
+
+ {/*
                         <form
                             action='https://getform.io/f/70877771-e309-418b-8fb8-d56d4a42203e' 
                             method="POST"
@@ -103,33 +190,4 @@ const Contact = () => {
                             </div>
                             <button className='w-full p-4 text-gray-100 mt-4' >Send Message</button>
                         </form>
-
-                    </div>
-                </div>
-            </div>
-            <div className='flex justify-center py-12' >
-                <Link href='/' >
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300' >
-                        <HiOutlineChevronDoubleUp className='m-auto text-[#5651e5]' size={30} />  
-                    </div>
-                </Link>
-            </div>
-        </div>
-    </div>
-  )
-}
-
-{/*<form
-        action='https://getform.io/f/70877771-e309-418b-8fb8-d56d4a42203e' 
-        method="POST"
-        enctype="multipart/form-data"
-        >
-        <input type='text' name='name' placeholder='Name' />
-        <input type='email' name='email' placeholder='Email' />
-        <input type='subject' name='subject' placeholder='Subject' />
-        <textarea name='message' cols='30' rows='10' placeholder='Message' />
-        <input type='file' name='resume[]' />
-        <button type='submit'>Submit</button>
-      </form> */}
-
-export default Contact
+ */}
