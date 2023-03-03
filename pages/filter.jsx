44f9b5451baa319f56bae86b5a75857df84b2ada
SALVADORPOETA <dsml@live.com.mx>
2023-03-03 17:11:59 -0600

    overview

--- pages/filter.jsx
+++ pages/filter.jsx
@@ -22,7 +22,7 @@ const filter = () => {
           <h2>Overview</h2>
           <p>
             This is a search filter built with ReactJS. The data was taken from Mockaroo. 
-            It is deployed on Vercel. It is responsive for mobile and tablet.
+            It is deployed on Vercel.
           </p>
           <a href='https://search-filter-sm.vercel.app/' target='_blank' ><button className='px-8 py-2 mt-4 mr-8' >Demo</button></a>
           <a href='https://github.com/SALVADORPOETA/Search-filter-sm' target='_blank' ><button className='px-8 py-2 mt-4' >Code</button></a>
