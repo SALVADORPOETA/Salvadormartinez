9fa2c3018e3f7ce26412dfc08196144cf2600e61
SALVADORPOETA <dsml@live.com.mx>
2023-03-03 17:39:06 -0600

    contactForm

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,6 +8,12 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import ContactImg from '../public/assets/contact.jpg';
 
 const Contact = () => {
+    const formRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        formRef.current.reset();
+    }
     return (
     <div id='contact' className='w-full lg:h-screen' >
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full' >
@@ -51,6 +57,7 @@ const Contact = () => {
                  <div className='col-span-3 h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4 ' >
                     <div className='p-4'>
                         <form
+                            ref={formRef}                            
                             id='form1'
                             action='https://getform.io/f/70877771-e309-418b-8fb8-d56d4a42203e' 
                             method="POST"
@@ -97,16 +104,11 @@ const Contact = () => {
                                     rows='10'
                                     name='message' />
                             </div>
-                            <div className='grid lg:grid-cols-2 gap-8' >
-                            <button 
-                                className='w-full flex-auto p-4 text-gray-100 mt-4' 
-                                >Send Message
-                            </button>
-                            <button 
-                                className='w-full p-4 mt-4 shadow-xl shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff] text-white' 
-                                type='reset'
-                                >Reset
-                            </button>
+                            <div className='grid lg:grid-cols-1 gap-8' >
+                                <button 
+                                    className='w-full flex-auto p-4 text-gray-100 mt-4' 
+                                    >Send Message
+                                </button>
                             </div>
                         </form>
                     </div>
@@ -124,72 +126,4 @@ const Contact = () => {
   );
 }
 
-
-
 export default Contact
-
-{/*
-<div className='flex items-center justify-center py-4'>
-    <a href='https://www.linkedin.com/in/salvador-mart%C3%ADnez-11211020a' target='_blank'>
-        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
-            <FaLinkedinIn />
-        </div>
-    </a>
-    <a href='https://github.com/SALVADORPOETA' target='_blank' >
-        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300' >
-            <FaGithub />
-        </div>
-    </a>
-</div>
- */}
-
- {/*
-                        <form
-                            action='https://getform.io/f/70877771-e309-418b-8fb8-d56d4a42203e' 
-                            method="POST"
-                            enctype="multipart/form-data"
-                            >
-                            <div className='grid md:grid-cols-2 gap-4 w-full py-2' >
-                                <div className='flex flex-col' >
-                                    <label className='uppercase text-sm py-2' >Name</label>
-                                    <input 
-                                        className='border-2 rounded-lg p-3 flex border-gray-300' 
-                                        type="text" 
-                                        name='name'
-                                    />
-                                </div>
-                                <div className='flex flex-col' >
-                                    <label className='uppercase text-sm py-2' >Phone Number</label>
-                                    <input 
-                                        className='border-2 rounded-lg p-3 flex border-gray-300' 
-                                        type="text"
-                                        name='phone' 
-                                    />
-                                </div>
-                            </div>
-                            <div className='flex flex-col py-2' >
-                            <label className='uppercase text-sm py-2' >Email</label>
-                            <input 
-                                className='border-2 rounded-lg p-3 flex border-gray-300' 
-                                type="email" 
-                                name='email'
-                            />
-                            </div>
-                            <div className='flex flex-col py-2' >
-                            <label className='uppercase text-sm py-2' >Subject</label>
-                            <input 
-                                className='border-2 rounded-lg p-3 flex border-gray-300' 
-                                type="text"
-                                name='subject' 
-                            />
-                            </div>
-                            <div className='flex flex-col py-2' >
-                            <label className='uppercase text-sm py-2' >Message</label>
-                            <textarea 
-                                className='border-2 rounded-lg p-3 border-gray-300' 
-                                rows='10'
-                                name='message' />
-                            </div>
-                            <button className='w-full p-4 text-gray-100 mt-4' >Send Message</button>
-                        </form>
- */}
