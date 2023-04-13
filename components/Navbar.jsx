2b275d47161a32ee1e4bcdf0eb3d649d5853b5a9
SALVADORPOETA <dsml@live.com.mx>
2023-04-13 00:30:39 -0600

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,6 @@ const Navbar = () => {
       router.asPath === '/navbar' ||
       router.asPath === '/githubauth' ||
       router.asPath === '/googleauth' ||
-      // router.asPath === '/googleauth' ||
       router.asPath === '/portfolio'
       ) 
       {
