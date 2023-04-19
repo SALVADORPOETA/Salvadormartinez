4a5b5a0c405cbe9e922bf33bbe367499bc321971
SALVADORPOETA <dsml@live.com.mx>
2023-04-19 09:27:08 -0600

    projects-py1

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -30,7 +30,7 @@ import navbarImg from '../public/assets/projects/navbar.png';
 const Projects = () => {
   return (
     <div id='projects' className='w-full mt-20' >
-        <div className='max-w-[1240px] mx-auto px-2 py-56' >
+        <div className='max-w-[1240px] mx-auto px-2' >
             <p className='text-xl tracking-widest uppercase text-[#5651e5]' >Projects</p>
             <h2 className='py-4' >Main Projects</h2>
             <div className='grid md:grid-cols-2 gap-8' >
