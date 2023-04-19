ad4f7f0e2f8b81b2705b43a516b81e9b7c7f0a2a
SALVADORPOETA <dsml@live.com.mx>
2023-04-19 09:18:46 -0600

    projects-mt

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -29,7 +29,7 @@ import navbarImg from '../public/assets/projects/navbar.png';
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full mt-20' >
+    <div id='projects' className='w-full mt-40' >
         <div className='max-w-[1240px] mx-auto px-2 py-16' >
             <p className='text-xl tracking-widest uppercase text-[#5651e5]' >Projects</p>
             <h2 className='py-4' >Main Projects</h2>
