6b5b53bcd087e6194f1dd4b002359ecc85f5ce17
SALVADORPOETA <dsml@live.com.mx>
2023-04-20 12:03:20 -0600

    firebasegoogle-tailwind

--- pages/firebaseemail.jsx
+++ pages/firebaseemail.jsx
@@ -40,7 +40,7 @@ const firebaseemail = () => {
                     <RiRadioButtonFill className='pr-1' />HTML
                 </p>
                 <p className='text-gray-600 py-2 flex items-center' >
-                    <RiRadioButtonFill className='pr-1' />CSS
+                    <RiRadioButtonFill className='pr-1' />Tailwind
                 </p>
                 <p className='text-gray-600 py-2 flex items-center' >
                     <RiRadioButtonFill className='pr-1' />JavaScript
