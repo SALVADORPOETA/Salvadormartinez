7521a2421a7ee5b9d01115a19e10eb7dfd927a7d
SALVADORPOETA <dsml@live.com.mx>
2023-05-03 23:16:22 -0600

    delete-navLogo+inter

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head';
-import { Inter } from '@next/font/google';
+// import { Inter } from '@next/font/google';
 import Main from '../components/Main';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import NavLogo from '../public/assets/navLogo.png'
+// import NavLogo from '../public/assets/navLogo.png'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
