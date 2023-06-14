81bf4bc261ddb18d56ed981e8d69122fdc801b05
SALVADORPOETA <dsml@live.com.mx>
2023-06-14 00:13:26 -0600

    h3-ReactJS

--- pages/navbar.jsx
+++ pages/navbar.jsx
@@ -12,7 +12,7 @@ const navbar = () => {
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={navbarImg} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2' >
           <h2 className='py-2' >Navbar</h2>
-          <h3>JavaScript</h3>
+          <h3>ReactJS</h3>
         </div>
       </div>
 
