a9db3545249d93b1f74e3b7c0e3e122ed5960ef7
SALVADORPOETA <dsml@live.com.mx>
2023-08-08 12:33:52 -0600

    financial.jsx h2

--- pages/financial.jsx
+++ pages/financial.jsx
@@ -11,7 +11,7 @@ const financial = () => {
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={financialImg} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2' >
-          <h2 className='py-2' >financial</h2>
+          <h2 className='py-2' >Financial Advisor</h2>
           <h3>ReactJS</h3>
         </div>
       </div>
