8d285f64edfc44960a26c500367e6787d3ec748b
SALVADORPOETA <dsml@live.com.mx>
2023-08-19 00:21:35 -0600

    drive-app-react-sm modification

--- pages/drive.jsx
+++ pages/drive.jsx
@@ -24,8 +24,8 @@ const drive = () => {
             This is a landing page about a car rent agency built with ReactJS and deployed in Vercel.
           </p>
           
-          <a href='https://drive-app-sm.vercel.app/' target='_blank' ><button className='px-8 py-2 mt-4 mr-8' >Demo</button></a>
-          <a href='https://github.com/SALVADORPOETA/drive-app-sm' target='_blank' ><button className='px-8 py-2 mt-4' >Code</button></a>
+          <a href='https://drive-app-react-sm.vercel.app/' target='_blank' ><button className='px-8 py-2 mt-4 mr-8' >Demo</button></a>
+          <a href='https://github.com/SALVADORPOETA/drive-app-react-sm' target='_blank' ><button className='px-8 py-2 mt-4' >Code</button></a>
           <Link href='/#projects' >
             <p className='underline cursor-pointer mt-20' >Back</p>
           </Link> 
