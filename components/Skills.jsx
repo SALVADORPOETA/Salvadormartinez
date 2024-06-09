e665d5e0b355a11b4773293e1915a870a43a2688
unknown <dsml@live.com>
2024-06-09 04:45:07 -0600

    skills - react

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -98,21 +98,6 @@ const Skills = () => {
                     <div className='grid grid-cols-2 gap-4 justify-center items-center' >
                         <div className='m-auto' >
                             <Image 
-                                src={ReactImg} 
-                                width='64' 
-                                height='64' 
-                                alt='/' 
-                            />
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>ReactJS</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300' >
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center' >
-                        <div className='m-auto' >
-                            <Image 
                                 src={NodeJS} 
                                 width='64' 
                                 height='64' 
@@ -124,6 +109,21 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>    
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300' >
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center' >
+                        <div className='m-auto' >
+                            <Image 
+                                src={ReactImg} 
+                                width='64' 
+                                height='64' 
+                                alt='/' 
+                            />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>ReactJS</h3>
+                        </div>
+                    </div>
+                </div>
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300' >
                     <div className='grid grid-cols-2 gap-4 justify-center items-center' >
                         <div className='m-auto' >
